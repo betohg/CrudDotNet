4784d32cbdb48be2bac7848bb556fcd4062772ad
LuisAl13 <122386195+LuisAl13@users.noreply.github.com>
2024-04-14 23:10:50 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoFinalCruds/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using ProyectoFinalCruds.Models;
+using System.Diagnostics;
+
+namespace ProyectoFinalCruds.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ProyectoFinalCruds/Models/Customers.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace ProyectoFinalCruds.Models
+{
+    
+
+        public class Customers
+        {
+            [Key]
+            [JsonPropertyName("customer_id")]
+            public int CUSTOMER_ID { get; set; }
+            [JsonPropertyName("name")]
+            public string? NAME { get; set; }
+            [JsonPropertyName("address")]
+            public string? ADDRESS { get; set; }
+            [JsonPropertyName("website")]
+            public string? WEBSITE { get; set; }
+            [JsonPropertyName("credit_limit")]
+            public decimal CREDIT_LIMIT { get; set; }
+
+        }
+    
+}
