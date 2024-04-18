9ea27af76a0ca0791a3d144306be5ce9ac3a7934
LuisAl13 <122386195+LuisAl13@users.noreply.github.com>
2024-04-18 00:00:00 -0600

    crud categorias

--- ProyectoFinalCruds/Data/ApplicationDbContext.cs
+++ ProyectoFinalCruds/Data/ApplicationDbContext.cs
@@ -13,24 +13,9 @@ namespace ProyectoFinalCruds.Data
         public DbSet<Customers> customers { get; set; }
         public DbSet<Contacts> contacts { get; set; }
         public DbSet<Orders> orders { get; set; }
-        ///public DbSet<Contacts> contacts { get; set; }
-        ///public DbSet<Countries> countries { get; set; }
-        ///public DbSet<Employees> employees { get; set; }
-        ///public DbSet<Inventories> inventories { get; set; }
-        ///public DbSet<Locations> locations { get; set; }
-        ///public DbSet<Order_Items> order_items { get; set; }
-        ///public DbSet<Orders> orders { get; set; }
-        ///public DbSet<Product_Categories> product_categories { get; set; }
-        ///public DbSet<Products> products { get; set; }
-        ///public DbSet<Regions> regions { get; set; }
-        ///public DbSet<Warehouses> warehouses { get; set; }
-        ///public DbSet<SqlProductosCategoria> sqlproductoscategoria { get; set; }
+        public DbSet<Products> products { get; set; }
+        public DbSet<Product_Categories> product_categories { get; set; }
 
-        ///[DbFunction(Schema = "dbo")]
-        ///public static int fn_PorductCategory_count(int pCategoryId)
-        ///{
-        ///	throw new Exception();
-        ///}
-
+        
     }
 }
---
+++ ProyectoFinalCruds/Models/Product_Categories.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace ProyectoFinalCruds.Models
+{
+    public class Product_Categories
+    {
+        [Key]
+        [JsonPropertyName("contact_id")]
+        public int? CATEGORY_ID { get; set; }
+       
+        [JsonPropertyName("category_name")]
+        public string? CATEGORY_NAME { get; set; }
+     
+
+    }
+}
